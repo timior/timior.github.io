f1163cab6a04e5e65a7950d4a79980a36eaadbb7
timior <tiamin@outlook.com>
2017-05-20 16:00:47 +0800

    Site updated: 2017-05-20 16:00:46

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Sat May 20 2017 15:30:40 GMT+0800 (中国标准时间)
+// build time:Sat May 20 2017 16:00:37 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
